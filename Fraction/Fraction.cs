4af37b4b7f54d38e6062e5d75f6b44cd5a1bc46e
Николай Немов <prost91@outlook.com>
2025-11-04 13:10:57 +0300

    Fraction DONE

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+	internal class Fraction
+	{
+		int integer; //целая часть
+		int numerator; //числитель
+		int denominator; //знаменатель
+
+		public int Integer { get; set; }
+		public int Numerator { get; set; }
+		public int Denominator { get; set; }
+
+		//	Constructors:
+
+		public Fraction()
+		{
+			this.Integer = 0;
+			this.Numerator = 0;
+			this.Denominator = 1;
+			Console.WriteLine($"DConstructor:\t{this.GetHashCode()}");
+		}
+		public Fraction(int integer)
+		{
+			this.Integer = integer;
+			this.Numerator = 0;
+			this.Denominator = 1;
+			Console.WriteLine($"SConstructor:\t{this.GetHashCode()}");
+		}
+		public Fraction(int numerator, int denominator)
+		{
+			this.Integer = 0;
+			this.Numerator = numerator;
+			this.Denominator = denominator;
+			Console.WriteLine($"Constructor:\t{this.GetHashCode()}");
+		}
+
+		public Fraction(int integer, int numerator, int denominator)
+		{
+			this.Integer = integer;
+			this.Numerator = numerator;
+			this.Denominator = denominator;
+			Console.WriteLine($"Constructor:\t{this.GetHashCode()}");
+		}
+
+		public Fraction(Fraction other)
+		{
+			this.Integer = other.Integer;
+			this.Numerator = other.Numerator;
+			this.Denominator = other.Denominator;
+			Console.WriteLine($"CopyConstructor:\t{this.GetHashCode()}");
+		}
+
+		~Fraction()
+		{
+			Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
+		}
+
+		// Operators:
+
+		public static Fraction operator +(Fraction left, Fraction right)
+		{
+			left.To_improper(left.Integer, left.Numerator, left.Denominator);
+			right.To_improper(right.Integer, right.Numerator, right.Denominator);
+			return new Fraction
+			(
+				left.Numerator * right.Denominator + right.Numerator * left.Denominator,
+				left.Denominator * right.Denominator
+			);
+		}
+		public static Fraction operator -(Fraction left, Fraction right)
+		{
+			left.To_improper(left.Integer, left.Numerator, left.Denominator);
+			right.To_improper(right.Integer, right.Numerator, right.Denominator);
+			return new Fraction
+			(
+				left.Numerator * right.Denominator - right.Numerator * left.Denominator,
+				left.Denominator * right.Denominator
+			);
+		}
+
+		public static Fraction operator *(Fraction left, Fraction right)
+		{
+			left.To_improper(left.Integer, left.Numerator, left.Denominator);
+			right.To_improper(right.Integer, right.Numerator, right.Denominator);
+
+			return new Fraction
+				(
+
+				left.Numerator * right.Numerator,
+				left.Denominator * right.Denominator
+				).Reduce();
+		}
+		public static Fraction operator /(Fraction left, Fraction right)
+		{
+			left.To_improper(left.Integer, left.Numerator, left.Denominator);
+			right.To_improper(right.Integer, right.Numerator, right.Denominator);
+
+			return left*right.Inverted(right.Integer, right.Numerator, right.Denominator);
+		}
+
+		public static Fraction operator ++(Fraction obj)
+		{
+			obj = obj.To_proper(obj.Integer, obj.Numerator, obj.Denominator);
+			obj.Integer++;
+			return obj;
+		}
+		public static Fraction operator --(Fraction obj)
+		{
+			obj = obj.To_proper(obj.Integer, obj.Numerator, obj.Denominator);
+			obj.Integer--;
+			return obj;
+		}
+
+		public static bool operator ==(Fraction left, Fraction right)
+		{
+			return
+				left.To_improper().Numerator * right.To_improper().Denominator == 
+				right.To_improper().Numerator * left.To_improper().Denominator;
+		}
+		public static bool operator !=(Fraction left, Fraction right)
+		{
+			return !(left == right);
+		}
+
+		public static bool operator >(Fraction left, Fraction right)
+		{
+			return
+				left.To_improper().Numerator * right.To_improper().Denominator > 
+				right.To_improper().Numerator * left.To_improper().Denominator;
+		}
+
+		public static bool operator <(Fraction left, Fraction right)
+		{
+			return
+				left.To_improper().Numerator * right.To_improper().Denominator <
+				right.To_improper().Numerator * left.To_improper().Denominator;
+		}
+
+
+
+		// Methods:
+
+		public Fraction To_improper()
+		{
+			Fraction copy = new Fraction(this);
+			copy.Numerator += copy.Integer * copy.Denominator;
+			copy.Integer = 0;
+			return copy;
+		}
+		public Fraction To_improper(int integer, int numerator, int denominator)
+		{
+			this.Numerator += integer * denominator;
+			this.Integer = 0;
+			return this;
+		}
+
+		public Fraction To_proper()
+		{
+			Fraction copy = new Fraction(this);
+			copy.Integer += copy.Numerator / copy.Denominator;
+			copy.Numerator %= copy.Denominator;
+			return copy;
+		}
+		public Fraction To_proper(int integer, int numerator, int denominator)
+		{
+			this.Integer += numerator / denominator;
+			this.Numerator %= denominator;
+			return this;
+		}
+
+		public Fraction Reduce()
+		{
+			int more, less, rest;
+			if (Numerator > Denominator)
+			{
+				more = Numerator;
+				less = Denominator;
+			}
+			else
+			{
+				less = Numerator;
+				more = Denominator;
+			}
+			do
+			{
+				rest = more % less;
+				more = less;
+				less = rest;
+			} while (rest > 0);
+			int GCD = more;
+			Numerator /= GCD;
+			Denominator /= GCD;
+			return this;
+		}
+
+		public Fraction Inverted()
+		{
+			Fraction inverted = new Fraction(this).To_improper();
+			(inverted.Numerator, inverted.Denominator) = (inverted.Denominator, inverted.Numerator);
+			return inverted;
+		}
+		public Fraction Inverted(int integer, int numerator, int denominator)
+		{
+			Fraction inverted = new Fraction(this).To_improper(this.Integer, this.Numerator, this.Denominator);
+			(inverted.Numerator, inverted.Denominator) = (inverted.Denominator, inverted.Numerator);
+			return inverted;
+		}
+
+		public void Info()
+		{
+			if (Denominator == 0)
+			{
+				Console.WriteLine("Error: The denominator must not be 0!");
+				return;
+			}
+			if (Integer != 0)
+			{
+				Console.Write(Integer);
+				if (Numerator != 0) Console.Write($"({Numerator}/{Denominator})\n");
+				else Console.WriteLine("");
+			}
+			else
+			{
+				if (Numerator != 0) Console.Write($"{Numerator}/{Denominator}\n");
+				else Console.WriteLine("");
+			}
+		}
+
+
+
+
+
+	}
+}
--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//#define CONSTRUCTORS_CHECK
+#define ARITHMETICAL_CHECK
+//#define COMPARISON_OPERATORS
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +13,79 @@ namespace Fraction
 {
 	internal class Program
 	{
+		static readonly string delimiter = "\n--------------------------------\n";
 		static void Main(string[] args)
 		{
+#if CONSTRUCTORS_CHECK
+			Fraction A = new Fraction();
+			A.Info();
+			Fraction B = new Fraction(5);
+			B.Info();
+			Fraction C = new Fraction(2, 5);
+			C.Info();
+			Fraction D = new Fraction(3, 2, 5);
+			D.Info();
+			Fraction E = new Fraction(D);
+			E.Info();
+#endif
+
+#if ARITHMETICAL_CHECK
+			Fraction A = new Fraction(2, 3, 4);
+			A.Info();
+			A.To_improper().Info();
+			A.To_improper(A.Integer, A.Numerator, A.Denominator).Info();
+			Console.WriteLine(delimiter);
+
+			Fraction B = new Fraction(3, 4, 5);
+			B.Info();
+			B.To_improper(B.Integer, B.Numerator, B.Denominator).Info();
+			Console.WriteLine(delimiter);
+
+			Fraction C = new Fraction(A);
+			C.Info();
+			C = A + B;
+			C.Info();
+			C.To_proper(C.Integer, C.Numerator, C.Denominator).Info();
+			Console.WriteLine(delimiter);
+
+			Fraction E = new Fraction(B - A);
+			E.Info();
+			E += A;
+			E.Info();
+			E.Reduce().Info();
+			E.To_proper(E.Integer, E.Numerator, E.Denominator).Info();
+			E.Inverted().Info();
+			E.Inverted(E.Integer, E.Numerator, E.Denominator).Info();
+			Console.WriteLine(delimiter);
+
+			E *= A;
+			E.Info();
+			E /= A;
+			E.Info();
+			Console.WriteLine(delimiter);
+
+			A.To_proper(A.Integer, A.Numerator, A.Denominator).Info();
+			Fraction D = new Fraction(A);
+			D.Info();
+			D++;
+			D.Info();
+			D--;
+			D.Info();
+#endif
+
+#if COMPARISON_OPERATORS
+			Fraction A = new Fraction(2, 3, 4);
+			A.Info();
+			Fraction B = new Fraction(3, 4, 5);
+			B.Info();
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine(A < B);
+			Console.WriteLine(A == B);
+			Console.WriteLine(A != B);
+			Console.WriteLine(A > B); 
+#endif
+
 		}
 	}
 }
